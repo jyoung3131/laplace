825224562ec9aab876f3d28b3a3daf8e6cfd1eb2
Nikhil Kulkarni <nikhil896@gmail.com>
2017-12-06 13:38:17 -0500

    Updated scripts

---
+++ jacobian/finalstep.sh
@@ -0,0 +1,1 @@
+ ld laplace2d-ae2eed.o laplace2d.o -o laplace-exec
--- jacobian/step5.sh
+++ jacobian/step5.sh
@@ -1,1 +1,1 @@
-/usr/local/cuda-8.0/bin/nvlink -report-arch -o laplace2d-nvidia -v -arch sm_35 -L/opt/ibm/clang-ykt/lib -L/usr/local/cuda-8.0/lib64 -L. -L/opt/ibm/clang-ykt/lib -lomptarget-nvptx laplace2d-514b33-f4b37d.cubin
+/usr/local/cuda-8.0/bin/nvlink -report-arch -o laplace2d-nvidia.out -v -arch sm_35 -L/opt/ibm/clang-ykt/lib -L/usr/local/cuda-8.0/lib64 -L. -L/opt/ibm/clang-ykt/lib -lomptarget-nvptx laplace2d-514b33-f4b37d.cubin
